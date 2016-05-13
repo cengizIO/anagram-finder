60f0bc78dec9de6ec095bb4bd815e7ebd5710d60
Cengiz Can <cengizc@gmail.com>
2016-05-13 03:40:39 +0300

    Use explicit lifetimes. Thanks goes to @mbrubeck

--- src/anagram.rs
+++ src/anagram.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-pub fn anagrams_for(source: &str, inputs: &[&str]) -> Vec<String> {
+pub fn anagrams_for<'a>(source: &str, inputs: &'a[&'a str]) -> Vec<&'a str> {
     let anagrams = process_inputs(inputs);
     let source_key = construct_key_from_word(&source);
 
@@ -10,13 +10,13 @@ pub fn anagrams_for(source: &str, inputs: &[&str]) -> Vec<String> {
     }
 }
 
-fn process_inputs(inputs: &[&str]) -> HashMap<String, Vec<String>> {
-    let mut anagrams = HashMap::<String, Vec<String>>::new();
+fn process_inputs<'a>(inputs: &'a [&'a str]) -> HashMap<String, Vec<&'a str>> {
+    let mut anagrams = HashMap::new();
 
     for word in inputs {
         let word_key = construct_key_from_word(word);
-        let mut words_list = anagrams.entry(word_key).or_insert(Vec::<String>::new());
-        words_list.push(String::from(word.clone()));
+        let mut words_list = anagrams.entry(word_key).or_insert(Vec::new());
+        words_list.push(*word);
     }
 
     anagrams
@@ -26,12 +26,5 @@ fn construct_key_from_word(word: &str) -> String {
     let mut chars: Vec<char> = word.chars().collect();
     chars.sort();
     chars.dedup();
-
-    let mut key_str = String::new();
-
-    for c in chars {
-        key_str.push(c);
-    }
-
-    key_str
+    chars.into_iter().collect()
 }
